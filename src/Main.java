80d90fa2c24498c0cd02e7057289b74536e48c63
Marcin <hulkcinek@gmail.com>
2020-02-12 20:25:26 +0100

    Proba naprawienia kodu

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         int n = s.nextInt();
         int m = s.nextInt();
         int licznik =0;
+        int ktoraLitera = 0;
         char[][] plansza = new char[n][m];
         String ciag = new String();
         for (int i = 0; i <= n; i++) {
@@ -23,10 +24,10 @@ public class Main {
             szukaneSlowa[i] = s.next();
         }
         for (int i = 0; i < szukaneSlowa.length; i++) {
-            if (szukanie(plansza, szukaneSlowa[i], n, m)){
-                System.out.println("TAK");
+            if (szukanie(plansza, szukaneSlowa[i], n, m, ktoraLitera, 0, 0)){
+                System.out.println(szukaneSlowa[i] + " TAK");
             }else {
-                System.out.println("NIE");
+                System.out.println(szukaneSlowa[i] + "NIE");
             }
         }
     }
@@ -35,44 +36,86 @@ public class Main {
     Do zastosowania rekurencja (aby obsluzyc np. wystepowanie tej samej literki u gory i na prawo
 
      */
-    public static boolean szukanie(char[][] plansza, String s, int n, int m) {
-        int x = 0;
-        int y = 0;
-        for (int a = 0; a < s.length(); a++) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    if (a == 0){
-                        if (s.charAt(a)==plansza[i][j]){
+    public static boolean szukanie(char[][] plansza, String s, int n, int m, int ktore, int x, int y) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (ktore == 0) {
+                    if (s.charAt(0) == plansza[i][j]) {
                             x = j;
                             y = i;
-                        }else if (i == n-1 && j == m-1){
+                            ktore++;
+                            szukanie(plansza, s, n, m, ktore, x, y);
+                    } else if (i == n - 1 && j == m - 1) {
                             return false;
-                        }
-                    }else {
-
-                        if(a == s.length()-1 && plansza[y-1][x] == s.charAt(a)){
-                            return true;
-                        }else if(a == s.length()-1 && plansza[y+1][x]==s.charAt(a)){
-                            return true;
-                        }else if(a == s.length()-1 && plansza[y][x-1]==s.charAt(a)){
-                            return true;
-                        }else if(a == s.length()-1 && plansza[y][x+1]==s.charAt(a)){
-                            return true;//
-                        }else if(plansza[y-1][x]==s.charAt(a)){
-                            y -= 1;
-                        }else if(plansza[y+1][x]==s.charAt(a)){
-                            y += 1;
-                        }else if(plansza[y][x-1]==s.charAt(a)){
-                            x -= 1;
-                        }else if(plansza[y][x+1]==s.charAt(a)){
-                            x += 1;
-                        }
                     }
+                } else {
+                        if (plansza[y-1][x] == ' ' ){//zamiast tej spacji powinien byc null??
+
+                        }
+
+
+
+
+
+//                        if (ktore == s.length() - 1 && plansza[y - 1][x] == s.charAt(a)) {
+//                            return true;
+//                        } else if (ktore == s.length() - 1 && plansza[y + 1][x] == s.charAt(a)) {
+//                            return true;
+//                        } else if (ktore == s.length() - 1 && plansza[y][x - 1] == s.charAt(a)) {
+//                            return true;
+//                        } else if (ktore == s.length() - 1 && plansza[y][x + 1] == s.charAt(a)) {
+//                            return true;//
+//                        } else if (plansza[y - 1][x] == s.charAt(a)) {
+//                            y -= 1;
+//                        } else if (plansza[y + 1][x] == s.charAt(a)) {
+//                            y += 1;
+//                        } else if (plansza[y][x - 1] == s.charAt(a)) {
+//                            x -= 1;
+//                        } else if (plansza[y][x + 1] == s.charAt(a)) {
+//                            x += 1;
                 }
             }
         }
         return false;
     }
+//    public static boolean szukanie(char[][] plansza, String s, int n, int m) {
+//        int x = 0;
+//        int y = 0;
+//        for (int a = 0; a < s.length(); a++) {
+//            for (int i = 0; i < n; i++) {
+//                for (int j = 0; j < m; j++) {
+//                    if (a == 0){
+//                        if (s.charAt(a)==plansza[i][j]){
+//                            x = j;
+//                            y = i;
+//                        }else if (i == n-1 && j == m-1){
+//                            return false;
+//                        }
+//                    }else {
+//
+//                        if(a == s.length()-1 && plansza[y-1][x] == s.charAt(a)){
+//                            return true;
+//                        }else if(a == s.length()-1 && plansza[y+1][x]==s.charAt(a)){
+//                            return true;
+//                        }else if(a == s.length()-1 && plansza[y][x-1]==s.charAt(a)){
+//                            return true;
+//                        }else if(a == s.length()-1 && plansza[y][x+1]==s.charAt(a)){
+//                            return true;//
+//                        }else if(plansza[y-1][x]==s.charAt(a)){
+//                            y -= 1;
+//                        }else if(plansza[y+1][x]==s.charAt(a)){
+//                            y += 1;
+//                        }else if(plansza[y][x-1]==s.charAt(a)){
+//                            x -= 1;
+//                        }else if(plansza[y][x+1]==s.charAt(a)){
+//                            x += 1;
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//        return false;
+//    }
 
     public static void wypisz(char[][] plansza, int m, int n) {
         for (int i = 0; i < n; i++) {
